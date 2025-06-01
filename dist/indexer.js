e52d3f1a87ea04546371d85facbfe004fa7bfdfb
Dang Hoang Lan <danghlambk14@gmail.com>
2025-06-01 09:49:42 +0700

    remove dist

--- dist/indexer.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-// Copyright (c) Mysten Labs, Inc.
-// SPDX-License-Identifier: Apache-2.0
-Object.defineProperty(exports, "__esModule", { value: true });
-const event_indexer_1 = require("./indexer/event-indexer");
-(0, event_indexer_1.setupListeners)();
